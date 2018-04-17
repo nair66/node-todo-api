4c6bd4d2110d2573de96ea60ac4e9c234c045c3c
aditya nair <adityanair66@outlook.com>
2018-04-17 20:23:16 +0530

    Delete route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;          //use built in promise in javascrpt
-// mongoose.connect('mongodb://aditya:todoapp@ds151228.mlab.com:51228/todoapp');
-mongoose.connect('mongodb://localhost:27017');
+mongoose.connect('mongodb://aditya:todoapp@ds151228.mlab.com:51228/todoapp');
+
 
 module.exports = { mongoose }
