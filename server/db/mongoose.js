6c9d714d481b02b017290d63988d3e18ccdca73e
aditya nair <31992429+nair66@users.noreply.github.com>
2021-08-12 19:34:26 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
+connectionStr = ''
 mongoose.Promise = global.Promise;          //use built in promise in javascrpt
-mongoose.connect('mongodb://aditya:todoapp@ds151228.mlab.com:51228/todoapp');
+mongoose.connect(connectionStr);
 
 
 module.exports = { mongoose }
